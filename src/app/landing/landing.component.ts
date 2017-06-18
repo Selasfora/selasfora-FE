425230fa327fe24e0d8f06b2126a6f0574497cb8
labibismaiel <labibismaiel@gmail.com>
2017-06-18 19:20:16 +0530

    small changes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  @Input() page = {
+  @Input() page: any = {
     colorShade: 'dark',
     imagePath: ''
   };
