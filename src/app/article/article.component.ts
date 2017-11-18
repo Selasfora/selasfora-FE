1135a9ad3698913ab39ad54442d397120c3550f9
Aman Gupta <amangupta.1990@ymail.com>
2017-11-18 13:02:58 +0530

    dynamic translations on article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {DynamicTranslationService} from "../dynamic-translation.service";
 import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -12,7 +13,7 @@ export class ArticleComponent implements OnInit {
   id: string = '';
   article: any = {};
   type:any;
-  constructor(public service: AuthService, public route: ActivatedRoute, private router: Router) { }
+  constructor(public service: AuthService, public route: ActivatedRoute, private router: Router, private dynamicTranslation: DynamicTranslationService) { }
 
   ngOnInit() {
     this.route.paramMap
@@ -23,8 +24,28 @@ export class ArticleComponent implements OnInit {
         that.service.fetchArticle(this.id)
         .subscribe(
           (data) => {
+
+            /** translate article */
+              /** ensure translations */
+          let parser = new DOMParser();
+          let translateText =[
+            data.title,
+            data.author,
+            data.tags,
+            data.body_html,
+          ]
+
+          that.dynamicTranslation.getTranslation(translateText).subscribe(res=>{
+            data.title = res[0][0];
+            data.author = res[0][1];
+            data.tags = res[0][2];
+            data.body_html = res[0][3];
             that.article = data;
             console.log('article', data);
+           
+          })
+
+            
           }
         );
       }
